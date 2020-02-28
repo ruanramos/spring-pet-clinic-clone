25ce25033d3d692e65d2a444c66eb392aadc34d8
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-02-28 03:31:31 -0300

    Spring initializr to start the project

---
+++ src/main/java/ruan/ramos/petclinicclone/PetClinicCloneApplication.java
@@ -0,0 +1,13 @@
+package ruan.ramos.petclinicclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ruan/ramos/petclinicclone/PetClinicCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package ruan.ramos.petclinicclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
