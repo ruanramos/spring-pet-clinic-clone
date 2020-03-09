4739b0f1a64572c70bb87ec0f600f8949998d22a
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-03-09 00:46:19 -0300

    Add base entity. Closes #10

--- pet-clinic-data/src/main/java/ruan/ramos/petclinicclone/model/Person.java
+++ pet-clinic-data/src/main/java/ruan/ramos/petclinicclone/model/Person.java
@@ -1,6 +1,6 @@
 package ruan.ramos.petclinicclone.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/ruan/ramos/petclinicclone/model/PetType.java
+++ pet-clinic-data/src/main/java/ruan/ramos/petclinicclone/model/PetType.java
@@ -1,6 +1,6 @@
 package ruan.ramos.petclinicclone.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
