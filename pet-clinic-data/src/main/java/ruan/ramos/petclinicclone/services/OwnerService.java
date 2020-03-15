0817f9e92dfbd6bdb38f693ec063492b17d46600
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-03-15 03:45:56 -0300

    Refactor to common crud interface, closes #10

--- pet-clinic-data/src/main/java/ruan/ramos/petclinicclone/services/OwnerService.java
+++ pet-clinic-data/src/main/java/ruan/ramos/petclinicclone/services/OwnerService.java
@@ -2,15 +2,7 @@ package ruan.ramos.petclinicclone.services;
 
 import ruan.ramos.petclinicclone.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
